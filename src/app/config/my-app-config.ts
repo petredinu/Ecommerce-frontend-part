7ae7be164997ac176f77bedb9e97138cefc755fb
Dinu Petre <dinu26petre@gmail.com>
2025-06-16 11:56:37 +0300

    Creeate config file for okta and install okta dependencies

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+export default {
+
+    oidc: {
+        clientId: '0oap5bdw06SiM54E45d7',
+        issuer: 'https://dev-30880913.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+
+    }
+}
+
