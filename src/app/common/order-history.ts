9125c71caa874dfc97db70e29237326c1c287418
petredinu <dinu_petre26@yahoo.ro>
2025-11-25 11:09:23 +0200

    feat(order-history): update OrderHistory model to include detailed customer and address information feat(orders-by-date): enhance order display with customer details, addresses, and improved UI elements

--- src/app/common/order-history.ts
+++ src/app/common/order-history.ts
@@ -1,13 +1,38 @@
 import { OrderItem } from './order-item';
 
 export class OrderHistory {
-
-    constructor( public id: string,
-                 public orderTrackingNumber: string,
-                 public totalPrice: number,
-                 public totalQuantity: number,
-                 public dateCreated: Date,
-                 public customer?: { email: string },
-                 public orderItems?: OrderItem[]) {
-    }
+    constructor(
+        public orderTrackingNumber: string,
+        public totalPrice: number,
+        public totalQuantity: number,
+        public dateCreated: Date,
+        public status: string,
+        // Proprietăți pentru relații
+        public customer: {
+            firstName: string;
+            lastName: string;
+            email: string;
+        },
+        public shippingAddress: {
+            street: string;
+            city: string;
+            state: string;
+            country: string;
+            zipCode: string;
+        },
+        public billingAddress: {
+            street: string;
+            city: string;
+            state: string;
+            country: string;
+            zipCode: string;
+        },
+        public orderItems: Array<{
+            imageUrl: string;
+            unitPrice: number;
+            quantity: number;
+            productId: number;
+            name: string;
+        }>
+    ) {}
 }
